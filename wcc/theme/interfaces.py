1c36b65000b13b0a8a775e29d58210a4e942cd6d
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-29 00:19:15 +0800

    added capabilities to select color scheme (re #147 , re #135)

---
+++ wcc/theme/interfaces.py
@@ -0,0 +1,7 @@
+from zope import schema
+from zope.interface import Interface
+
+class IThemeSettings(Interface):
+    scheme = schema.Choice(title=u"Color scheme",
+            vocabulary="wcc.theme.colorscheme",
+            default='red')
